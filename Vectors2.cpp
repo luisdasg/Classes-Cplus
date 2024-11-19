a4eab8d76c9cd7af5ae431394dcf088fd371850a
luisdasg <luisusa2312@gmail.com>
2024-11-18 20:01:31 -0600

    Add files via upload

--- Vectors2.cpp
+++ Vectors2.cpp
@@ -4,29 +4,28 @@ using namespace std;
 
 int main()
 {
-	int n;
-	vector<vector<int>> numbers; //3 rows by 4 colums
-	vector<int> temp1;
+	int num;
+	vector<vector<int>> numbers;
+	vector<int> temp;
 
 	for (int r = 0; r < 3; r++)
 	{
 		for (int c = 0; c < 4; c++)
 		{
-			cout << "Enter row # " << (r + 1) << " colums # " << (c + 1) << " ==> ";
-			cin >> n;
-			temp1.push_back(n);
+			cout << "Enter row # " << (r + 1) << " columns # " << (c + 1) << " --> ";
+			cin >> num;
+			temp.push_back(num);
 		}
-		numbers.push_back(temp1);
-		temp1.clear();
+		numbers.push_back(temp);
+		temp.clear();
 		cout << endl;
 	}
 	for (int r = 0; r < numbers.size(); r++)
 	{
-		for (int c = 0; c < numbers[r].size(); c++)
+		for (int c=0; c<numbers[r].size(); c++)
 		{
 			cout << numbers[r][c] << "\t";
 		}
 		cout << endl;
 	}
-
 }
